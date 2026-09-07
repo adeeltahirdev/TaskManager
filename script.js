8d11c5473fa57e025ca0586763219ff6d705eb99
adeeltahirdev <adeeltahir0301@gmail.com>
2026-09-07 18:28:22 +0500

    Added edit/del btns

--- script.js
+++ script.js
@@ -1,37 +1,79 @@
 const addtask = document.getElementById('add')
 const submit = document.getElementById('submit')
 
+
 function add_task() {
+
     const task = addtask.value
     const tasks = JSON.parse(localStorage.getItem('task')) || []
+
     if (task) {
+
         tasks.push(task)
+
         const taskList = document.getElementById('taskList')
+
         const taskItem = document.createElement('li')
-        taskItem.textContent = task
+        taskItem.classList.add('task-item')
+
+        taskItem.innerHTML = `
+            <span>${task}</span>
+
+            <div>
+                <button class="edit-btn">Edit</button>
+                <button class="delete-btn">Delete</button>
+            </div>
+        `
+
         taskList.appendChild(taskItem)
+
         addtask.value = ''
+
         const message = document.querySelector('.message')
+
         if (message) {
             message.remove()
         }
+
         localStorage.setItem('task', JSON.stringify(tasks))
     }
 }
 
-let tasks = JSON.parse(localStorage.getItem('task'))
+
+const tasks = JSON.parse(localStorage.getItem('task'))
+
 if (tasks) {
+
     const tasklist = document.getElementById('taskList')
+
     tasks.forEach(task => {
+
         const taskitem = document.createElement('li')
-        taskitem.textContent = task
+        taskitem.classList.add('task-item')
+
+        taskitem.innerHTML = `
+            <span>${task}</span>
+
+            <div>
+                <button class="edit-btn">Edit</button>
+                <button class="delete-btn">Delete</button>
+            </div>
+        `
+
         tasklist.appendChild(taskitem)
     })
+
 }
+
 else {
+
     const task = document.getElementById('display')
+
     const message = document.createElement('p')
+
     message.classList.add('message')
+
     message.textContent = 'Add a task to get started!'
+
     task.appendChild(message)
 }
